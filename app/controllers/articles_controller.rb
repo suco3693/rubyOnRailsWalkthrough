3b311209df5e8d554987629328cbe050e79033fd
Sutton <suco3693@gmail.com>
2020-06-17 13:34:23 -0700

    5.13 Base Article CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
     def show
         @article = Article.find(params[:id])
     end
